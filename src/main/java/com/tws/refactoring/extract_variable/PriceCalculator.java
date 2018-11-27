ae01c3827b0cd447c7d538fde25cedd3c2e4b62f
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-27 10:19:18 +0800

    Refactoring getBasePrice

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -3,8 +3,12 @@ package com.tws.refactoring.extract_variable;
 public class PriceCalculator {
     public double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
+        return getBasePrice(quantity, itemPrice) -
                 Math.max(0, quantity - 500) * itemPrice * 0.05 +
                 Math.min(quantity * itemPrice * 0.1, 100.0);
+    }
+
+    private int getBasePrice(int quantity, int itemPrice) {
+        return quantity * itemPrice;
     }
 }
