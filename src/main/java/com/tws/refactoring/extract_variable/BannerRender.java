4c90ef62dee055adf80e64810d287308cfbe4150
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-27 09:19:10 +0800

    refactoring matchPlatform

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,7 +2,8 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     void renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
+        boolean matchPlatform = platform.toUpperCase().indexOf("MAC") > -1;
+        if (matchPlatform &&
                 (browser.toUpperCase().indexOf("IE") > -1)) {
             // do something
             System.out.print("true");
